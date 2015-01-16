12a918416ab30ae44eae166a2d791f35f7d7dc66
bddicken <bddicken@gmail.com>
2015-01-15 20:50:12 -0700

    add sql files

---
+++ scripts/resources/load_data.sql
@@ -0,0 +1,3 @@
+COPY playbyplay FROM './../download/AllData2011playoffs20111224./playbyplay201106122333.txt' WITH (format csv, delimiter E'\t')
+;
+
---
+++ scripts/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE playbyplay (
+    GameID varchar(32),
+    LineNumber varchar(16),
+    TimeRemaining varchar(32),
+    Entry varchar
+)
+;
+

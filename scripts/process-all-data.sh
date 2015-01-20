8d58d86c1c9ce5867a04ab04e0f7c4467f224a0a
bddicken <bddicken@gmail.com>
2015-01-19 20:29:07 -0700

    add script to process all data. Totals about 4.5 million entries

---
+++ scripts/process-all-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATA_ROOT_DIR="${1}"
+
+PBP_FILES=$(find . "${DATA_ROOT_DIR}" | grep 'playoff\|reg' | grep 'playbyplay')
+
+echo ${PBP_FILES}
+
+for DATA_FILE in ${PBP_FILES}; do
+    python process-data.py -i ${DATA_FILE} -s "SEASON"
+done
+
--- scripts/process-data.py
+++ scripts/process-data.py
@@ -1,36 +1,65 @@
 #
 # A first crack at parsing the data into reasonable information
 #
-# TODO: lots of things!
 
 import csv
 import argparse
 
 optionsParser = argparse.ArgumentParser()
 
-optionsParser.add_argument('--inputFile', '-i', required=True, default='./input.xtxt', help="Input file")
+optionsParser.add_argument('--inputFile', '-i', required=True, default='./input.txt', help="Input file")
+optionsParser.add_argument('--season', '-s', required=True, default='./input.txt', help="Format XXXX-XXXX")
+
 args = optionsParser.parse_args()
 
 def PrintOptions():
     print "output: " + args.config
+
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
 
 def loadData():
     print args.inputFile
     file = open(args.inputFile)
     reader = csv.reader(file, delimiter="\t")
     d = list(reader)
+
     for row in d:
-        event = row[3]
+
+        gameID = row[0]
+        seqID = row[1]
         time = row[2]
+        event = row[3]
         tokens = event.split()
         playerName = ""
         teamID = "" 
         eventType = "" 
-        specificEventType = ""
-        
+        specificEventType = "NA"
+        season = args.season
+
+        # parse data in/near brackets
+        for token in tokens:
+            if "[" not in token and "]" not in token:
+                playerName = token
+                break
+            elif "[" in token:
+                teamID = token.replace("[", "")
+                teamID = teamID.replace("]", "")
+
         # Process gneral event info
         for token in tokens:
             #print token
+            if "Substitution" in token:
+                eventType = "Sub"
+                break
+            if "Throw" in token:
+                eventType = "FT"
+                break
             if "Turnover" in token:
                 eventType = "TO"
                 break
@@ -45,25 +74,18 @@ def loadData():
                 break
             elif "Shot" in token:
                 eventType = "Shot"
+                specificEventType = find_between(event, playerName, "Shot")
 
-        # parse data in/near brackets
-        for token in tokens:
-            if "[" not in token and "]" not in token:
-                playerName = token
-                break
-            elif "[" in token:
-                teamID = token.replace("[", "")
-                teamID = teamID.replace("]", "")
-
-        #if "Shot" in event:
-        #    if "Made" in event:
-        #        print time + "::" + teamID + " " + playerName + " made a shot"
-        #    else:
-        #        print time + "::" + teamID + " " + playerName + " missed a shot"
         if eventType:
-            print time + "::" + teamID + " " + playerName + " executed action: " + eventType
-        #else:
-        #    print "unknown action"
+            print                       \
+                    gameID + "\t" +     \
+                    seqID + "\t" +      \
+                    season + "\t" +     \
+                    time + "\t" +       \
+                    teamID + "\t" +     \
+                    playerName + "\t" + \
+                    eventType + "\t" +  \
+                    specificEventType
 
 def main():
     loadData()

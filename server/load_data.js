e25e11c1f5c75133d804adce841f1a7f298af410
bddicken <bddicken@gmail.com>
2015-04-01 18:26:45 -0700

    update load script

--- server/load_data.js
+++ server/load_data.js
@@ -40,6 +40,7 @@ if (
     console.log("must specify all arguments.");
 }
 
+var maxReturn = 99999999;
 
 var getFirstValue = function (d) {
     var kk = Object.keys(d).sort();
@@ -51,138 +52,132 @@ var getFirstKey = function (d) {
     return kk[0];
 }
 
-gameEventCount = 0;
-playerCount = 0;
+var fileCount = 6;
+var checkFinished = function(rowCount, rowType) {
+    if (rowCount == 0) {
+        console.log("Done processing " + rowType + " elements.");
+        fileCount--;
+    }
+    if (fileCount == 0) {
+        console.log("Done processing all Files. Exiting.");
+        process.exit(code=0);
+    }
+}
 
-var playerIO = readline.createInterface({
-    input: fs.createReadStream(program.players),
-    output: process.stdout,
-    terminal: false
-});
+///
+/// Players
+///
 
-playerIO.on('close', function(line) {
-    console.log("Done processing players."); 
-});
-
-playerIO.on('line', function(line) {
+var playerStream = fs.createReadStream(program.players);
+var playerCount = 1;
+var playerCSVStream = csv()
+.on('end', function(line) {
+    console.log("Done processing players"); 
+    playerCount--;
+    checkFinished(playerCount, "Player");
+})
+.on('data', function(line) {
     raw = JSON.parse(line);
 
-    var playerID = raw.playerID;
-    var name     = raw.name;
-    var fullName = raw.trueName;
-    
-    //console.log("trying to create new player");
-
     var player = new Player(); 
-    player.name = name;
-    player.fullName = fullName;
-    player.age = 0;
+    player.name = raw.playerID;
+    player.fullName = raw.name;
+    player.age = raw.trueName;
+    
+    playerCount++;
 
     player.save(function(err) {
         if (err) { console.log('FAILED -> ' + err); }
-        else { 
-            //console.log((playerCount++) + " player created."); 
-        }
+        playerCount--;
+        checkfinished(playerCount, "player");
     });
 });
+playerStream.pipe(playerCSVStream);
 
-var gameIO = readline.createInterface({
-    input: fs.createReadStream(program.games),
-    output: process.stdout,
-    terminal: false
-});
+///
+///
+///
 
-gameIO.on('close', function(line) {
-    console.log("Done processing games."); 
-});
+///
+/// Team
+///
 
-gameIO.on('line', function(line) {
-    raw = JSON.parse(line);
-
-    var game = new Game(); 
-    
-    game.name       = raw.name;
-    game.season     = raw.season;
-    game.homeTeam   = raw.homeTeam;
-    game.awayTeam   = raw.awayTeam;
-    game.date       = raw.date;
-    game.id         = raw.id;
-
-    game.save(function(err) {
-        if (err) { console.log('FAILED -> ' + err); }
-        else { 
-            //console.log((playerCount++) + " game created."); 
-        }
-    });
-});
-
-var teamIO = readline.createInterface({
-    input: fs.createReadStream(program.teams),
-    output: process.stdout,
-    terminal: false
-});
-
-teamIO.on('close', function(line) {
-    console.log("Done processing teams."); 
-});
-
-teamIO.on('line', function(line) {
+var teamStream = fs.createReadStream(program.teams);
+var teamCount = 1;
+var teamCSVStream = csv()
+.on('end', function(line) {
+    console.log("Done processing teams"); 
+    teamCount--;
+    checkFinished(teamCount, "Player");
+})
+.on('data', function(line) {
     raw = JSON.parse(line);
 
     var team = new Team(); 
-    
     team.name        = raw.name;
     team.id          = raw.id;
+    
+    teamCount++;
 
     team.save(function(err) {
         if (err) { console.log('FAILED -> ' + err); }
-        else { 
-            //console.log((playerCount++) + " team created."); 
-        }
+        teamCount--;
+        checkfinished(teamCount, "team");
     });
 });
+teamStream.pipe(teamCSVStream);
 
-var seasonIO = readline.createInterface({
-    input: fs.createReadStream(program.seasons),
-    output: process.stdout,
-    terminal: false
-});
+///
+///
+///
 
-seasonIO.on('close', function(line) {
-    console.log("Done processing seasons."); 
-});
+///
+/// Season
+///
 
-seasonIO.on('line', function(line) {
+var seasonStream = fs.createReadStream(program.seasons);
+var seasonCount = 1;
+var seasonCSVStream = csv()
+.on('end', function(line) {
+    console.log("Done processing seasons"); 
+    seasonCount--;
+    checkFinished(seasonCount, "Player");
+})
+.on('data', function(line) {
     raw = JSON.parse(line);
 
     var season = new Season(); 
-    
     season.name        = raw.name;
     season.id          = raw.id;
+    
+    seasonCount++;
 
     season.save(function(err) {
         if (err) { console.log('FAILED -> ' + err); }
-        else { 
-            //console.log((playerCount++) + " season created."); 
-        }
+        seasonCount--;
+        checkfinished(seasonCount, "season");
     });
 });
+seasonStream.pipe(seasonCSVStream);
 
-var gameEventIO = readline.createInterface({
-    input: fs.createReadStream(program.gameEvents),
-    output: process.stdout,
-    terminal: false
-});
+///
+///
+///
 
-gameEventIO.on('close', function(line) {
-    console.log("Done processing gameEvents."); 
-    buildSummaries();
-});
+///
+/// GameEvent
+///
 
-gameEventIO.on('line', function(line) {
+var gameEventStream = fs.createReadStream(program.gameEvents);
+var gameEventCount = 1;
+var gameEventCSVStream = csv()
+.on('end', function(line) {
+    console.log("Done processing gameEvents"); 
+    gameEventCount--;
+    checkFinished(gameEventCount, "Player");
+})
+.on('data', function(line) {
     raw = JSON.parse(line);
-    
-    var pbpCount = 0;
 
     var mongoPlayerID;
     var gameID      = raw.gameID;
@@ -212,14 +207,57 @@ gameEventIO.on('line', function(line) {
     gameEvent.eventType         = eventType;
     gameEvent.eventDetail       = eventDetail;
     gameEvent.secondsIntoGame   = timeInSecs;
+    
+    gameEventCount++;
 
     gameEvent.save(function(err) {
         if (err) { console.log('FAILED -> ' + err); }
-        else { 
-            //console.log((pbpCount++) + " pbp event created."); 
-        }
+        gameEventCount--;
+        checkfinished(gameEventCount, "gameEvent");
     });
 });
+gameEventStream.pipe(gameEventCSVStream);
+
+///
+///
+///
+
+///
+/// Game
+///
+
+var gameStream = fs.createReadStream(program.games);
+var gameCount = 1;
+var gameCSVStream = csv()
+.on('end', function(line) {
+    console.log("Done processing games"); 
+    gameCount--;
+    checkFinished(gameCount, "Player");
+})
+.on('data', function(line) {
+    raw = JSON.parse(line);
+
+    var game = new Game(); 
+    game.name       = raw.name;
+    game.season     = raw.season;
+    game.homeTeam   = raw.homeTeam;
+    game.awayTeam   = raw.awayTeam;
+    game.date       = raw.date;
+    game.id         = raw.id;
+
+    gameCount++;
+
+    game.save(function(err) {
+        if (err) { console.log('FAILED -> ' + err); }
+        gameCount--;
+        checkfinished(gameCount, "game");
+    });
+});
+gameStream.pipe(gameCSVStream);
+
+///
+///
+///
 
 var summarizeGameEvents = function(ge, groupBy) {
     
@@ -292,8 +330,7 @@ var summarizeGameEvents = function(ge, groupBy) {
     return summaryArray; 
 }
 
-var maxReturn = 99999999;
-
+var summaryCount = 1;
 var buildSummaries = function() {
     var query = GameEvent.find().limit(maxReturn);
     var groupBy = 'player';
@@ -305,22 +342,26 @@ var buildSummaries = function() {
             var keyData = getFirstKey(summary).split(',');
 
             var s = new Summary();
+            
             //s.name = getFirstKey(summary);
+
             s.minutes = getFirstValue(summary);
             s.season  = keyData[0];
             s.team    = keyData[1];
             s.player  = keyData[2];
-            console.log(s.season + " " + s.player + " " + s.team)
+            
+            //console.log(s.season + " " + s.player + " " + s.team)
+            
+            summaryCount++;
 
             s.save(function(err) {
                 if (err) { console.log('FAILED -> ' + err); }
+                summaryCount--;
+                checkfinished(summaryCount, "summary");
             });
         }
-        console.log("done processing summaries");
-
-        // Cannot exit right away, because the "s.save" callbacks may not have completed.
-        // Update this to use promises, etc
-        //process.exit(code=0);
+        summaryCount--;
+        checkfinished(summaryCount, "summary");
     });
 }
 
